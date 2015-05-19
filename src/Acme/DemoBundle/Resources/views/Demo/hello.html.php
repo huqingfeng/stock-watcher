bbc505e19703323c3c621d949c613d6cd44baee0
Alex Hu <alexhu@hpn.com>
2015-05-19 17:57:51 -0500

    call jquery and bootstrap libs

--- src/Acme/DemoBundle/Resources/views/Demo/hello.html.php
+++ src/Acme/DemoBundle/Resources/views/Demo/hello.html.php
@@ -1,11 +1,14 @@
 
-        <link rel="stylesheet" href="lib/css/bootstrap.css">
-        <script src="lib/javascript/jquery-1.11.2.min.js"></script>
-        <script src="lib/javascript/bootstrap.js"></script>
+<!--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>-->
+
+        <link rel="stylesheet" href="/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/css/main.css" />
 
 
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.6/css/jquery.dataTables.min.css"></style>
-        <script type="text/javascript" src="https://cdn.datatables.net/1.10.6/js/jquery.dataTables.min.js"></script>
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
 
 
         <script type="text/javascript">
