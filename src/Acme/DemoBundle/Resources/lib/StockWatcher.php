030b7279a132506b15a34ea7590bf324b7f69c16
Alex Hu <alexhu@hpn.com>
2016-04-14 12:50:26 -0500

    add more stock symbols

--- src/Acme/DemoBundle/Resources/lib/StockWatcher.php
+++ src/Acme/DemoBundle/Resources/lib/StockWatcher.php
@@ -19,6 +19,9 @@ class StockWatcher
     public function getStockSymbols()
     {
         return array(
+                'SPY',
+                'QQQ',
+                'USO',
                 'AAPL',
                 'WUBA',
                 'QIHU',
@@ -33,7 +36,9 @@ class StockWatcher
                 'BABA',
                 'SPY',
                 'QQQ',
-                'MOMO'
+                'MOMO',
+                'YOKU',
+                'TSLA',
             );
     }
 
